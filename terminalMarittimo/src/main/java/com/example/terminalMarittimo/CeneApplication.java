487236d8e2acc884a259bad3098044e375fb4dff
JEANMONNET\cenedese_davide <davicene333@gmail.com>
2025-05-09 11:28:09 +0200

    Base

---
+++ terminalMarittimo/src/main/java/com/example/terminalMarittimo/CeneApplication.java
@@ -0,0 +1,13 @@
+package com.example.terminalMarittimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeneApplication.class, args);
+	}
+
+}
---
+++ terminalMarittimo/src/test/java/com/example/terminalMarittimo/CeneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.terminalMarittimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
